759644c7dff3e2be9504b44343762c5bc2f38041
Sabrineh <ksabrineh@gmail.com>
2024-05-30 19:17:41 +0330

    fix design of inputs

--- tools/Input.js
+++ tools/Input.js
@@ -1,11 +1,59 @@
 "use client";
-import { TextField } from "@mui/material";
+import { AccountCircle } from "@mui/icons-material";
+import { InputAdornment } from "@mui/material";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 import React from "react";
 
-const Input = ({ lable = "",className }) => {
+const Input = ({ lable = "", className, icon, infoIcon, type }) => {
   return (
     <div className="w-full">
-      <TextField label={lable} className={`text-black ${className && className}`} focused />
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch", height: "15px" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          type={type ? type : "text"}
+          label={lable}
+          className={`text-black focus:!border-none ${className && className}`}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="start">
+                {infoIcon && infoIcon}
+              </InputAdornment>
+            ),
+            startAdornment: (
+              <InputAdornment position="start">{icon && icon}</InputAdornment>
+            ),
+          }}
+          sx={{
+            "& .MuiInputBase-root": {
+              height: 30,
+              borderRadius: "5px",
+              margin: "0px",
+            },
+            "& .MuiOutlinedInput-root": {
+              "&.Mui-focused fieldset": {
+                borderColor: "gray",
+              },
+              "&:hover fieldset": {
+                borderColor: "gray",
+              },
+            },
+            "& .MuiInputLabel-root": {
+              "&.Mui-focused": {
+                color: "black", 
+              },
+            },
+          }}
+        />
+      </Box>
     </div>
   );
 };
